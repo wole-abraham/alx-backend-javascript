cc030aaa28ed96f876c00a10c8695ad2d081579d
abraham <wole1702@gmail.com>
2024-10-01 01:52:53 +0100

    code

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gpd, capita) {
   const budget = getBudgetObject(income, gpd, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: (income) => `${income} euros`,
+    getIncomeInDollars(income) { return `$${income}`},
+    getIncomeInEuros(income) { return  `${income} euros`}
   };
   return fullBudget;
 }
