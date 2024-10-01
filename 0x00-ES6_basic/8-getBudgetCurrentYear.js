f46a5c349350655e272922fe6c4c4d9edfb44ac3
abraham <wole1702@gmail.com>
2024-10-01 01:33:02 +0100

    code

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,6 +1,6 @@
 function getCurrentYear() {
   const date = new Date();
-  return `-${date.getFullYear()};`;
+  return `-${date.getFullYear()}`;
 }
 export default function getBudgetCurrentYear(income, gdp, capita) {
   const budget = {
