190508fa1dc9344b0b3b13aba1ca123df80c661b
abraham <wole1702@gmail.com>
2024-10-03 00:16:10 +0100

    code

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() =>  { 
-		  return {status: 200, body: 'success' } 
-  });
+  promise.then(() => ({ status: 200, body: 'success' }));
 
-  promise.catch(() => {
-		  return new Error()
-  });
+  promise.catch(() => new Error());
 
   promise.finally(() => {
     console.log('Got a response from the API');
