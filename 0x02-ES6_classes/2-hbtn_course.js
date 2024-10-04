229c9d3c77eb180cbd2cf86afc112cf3cfa2903a
abraham <wole1702@gmail.com>
2024-10-04 21:23:23 +0100

    code

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,65 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name; // eslint-disable-line no-underscore-dangle
+    } else {
+      throw new TypeError('Name must be a String');
+    }
+
+    if (typeof (length) === 'number') {
+      this._length = length; // eslint-disable-line no-underscore-dangle
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (Array.isArray(students)) {
+      for (const i in students) {
+        if (typeof (i) !== 'string') {
+          throw new TypeError('Students must be an array of strings');
+        } else {
+          this._students = students; // eslint-disable-line no-underscore-dangle
+        }
+      }
+    }
+  }
+
+  get name() {
+    return this._name; // eslint-disable-line no-underscore-dangle
+  }
+
+  get length() {
+    return this._length; // eslint-disable-line no-underscore-dangle
+  }
+
+  get students() {
+    return this._students; // eslint-disable-line no-underscore-dangle
+  }
+
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value; // eslint-disable-line no-underscore-dangle
+  }
+
+  set length(value) {
+    if (typeof (value) !== 'number') {
+      throw new TypeError('Length must be a nummber');
+    }
+    this._length = value; // eslint-disable-line no-underscore-dangle
+  }
+
+  set students(value) {
+    if (Array.isArray(value)) {
+      for (const i in value) {
+        if (typeof (i) !== 'string') {
+          throw new TypeError('Student must be a array of strings');
+        } else {
+          this._students = value; // eslint-disable-line no-underscore-dangle
+        }
+      }
+    } else {
+      throw new TypeError('Students must be a array of strings');
+    }
+  }
+}
