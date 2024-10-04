8ee4ad26924b0dae9227e616a001a8bae393f9ae
abraham <wole1702@gmail.com>
2024-10-04 21:44:39 +0100

    code

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,36 +1,35 @@
 export default class Currency {
-		constructor(code, name){
-				if (typeof(code) !== 'string' && typeof(name) !== 'string'){
-						throw new TypeError("Code and Name must be string");
-				}
-				this._code = code; // eslint-disable-line no-underscore-dangle
-				this._name = name; // eslint-disable-line no-underscore-dangle
-		}
+  constructor(code, name) {
+    if (typeof (code) !== 'string' && typeof (name) !== 'string') {
+      throw new TypeError('Code and Name must be string');
+    }
+    this._code = code; // eslint-disable-line no-underscore-dangle
+    this._name = name; // eslint-disable-line no-underscore-dangle
+  }
 
-		set name(value){
-				if (typeof(value) !== 'string'){
-						throw new TypeError('Name must be a string');
-				}
-				this._name = value;
-		}
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value; // eslint-disable-line no-underscore-dangle
+  }
 
-		get name() {
-				return this._name;
-		}
-	
-		set code(value){
-				if (typeof(value) !== 'string'){
-						throw new TypeError('Name must be a string');
-				}
-				this._code = code;
-		}
+  get name() {
+    return this._name; // eslint-disable-line no-underscore-dangle
+  }
 
-		get code(){
-				return this._code;
-		}
+  set code(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = value; // eslint-disable-line no-underscore-dangle
+  }
 
-		displayFullCurrency(){
-				return `${this._name} (${this._code})`;
-		}
+  get code() {
+    return this._code; // eslint-disable-line no-underscore-dangle
+  }
 
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`; // eslint-disable-line no-underscore-dangle
+  }
 }
