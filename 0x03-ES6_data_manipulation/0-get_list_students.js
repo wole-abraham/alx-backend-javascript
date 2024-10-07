0d3a3e4e995ab04a8be78412f94381ee85ccc06b
abraham <wole1702@gmail.com>
2024-10-07 16:56:30 +0100

    code

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
-export default function getListStudent(){
-		const array = [{id: 1, firstName: 'Guillaume', location:'San Francisco'},
-				{id: 2, firstname: 'James', location: 'Columbia'},
-				{id: 5, firstName: 'Serena', location: 'San Francisco'}
-		]
+export default function getListStudent() {
+  const array = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 
-		return array;
+  return array;
 }
