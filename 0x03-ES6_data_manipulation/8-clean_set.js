d0236aefb4103ebb3a4406f2ca2e2976b8cda5fc
abraham <wole1702@gmail.com>
2024-10-09 00:50:41 +0100

    code

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+		if (startString.length > 0){
+				
+		const res = [];
+		set.forEach((value) => {
+				if (value.startsWith(startString)){
+						res.push(value.slice(startString.length));
+				}
+		});
+		return res.join('-');
+		}
+}
