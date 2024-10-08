a5af7e1bb75b7e09388d8fa530a37267c0a23598
abraham <wole1702@gmail.com>
2024-10-09 00:52:19 +0100

    code

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,13 @@
 export default function cleanSet(set, startString) {
-		if (startString.length > 0){
-				
-		const res = [];
-		set.forEach((value) => {
-				if (value.startsWith(startString)){
-						res.push(value.slice(startString.length));
-				}
-		});
-		return res.join('-');
-		}
-		else {
-				return '';
-		}
+  if (startString.length > 0) {
+    const res = [];
+    set.forEach((value) => {
+      if (value.startsWith(startString)) {
+        res.push(value.slice(startString.length));
+      }
+    });
+    return res.join('-');
+  }
+
+  return '';
 }
