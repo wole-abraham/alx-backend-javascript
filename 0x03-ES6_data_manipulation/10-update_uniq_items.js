4f24409930a94a0344284d5bf9ea0e17c5e3bf2b
abraham <wole1702@gmail.com>
2024-10-09 01:10:23 +0100

    code

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(map) {
+  const maps = map;
+  if (maps instanceof Map) {
+    for (const key of maps.keys()) {
+      if (maps.get(key) === 1) {
+        maps.set(key, 100);
+      }
+    }
+  } else {
+    throw new Error('Cannot Process');
+  }
+  return maps;
+}
