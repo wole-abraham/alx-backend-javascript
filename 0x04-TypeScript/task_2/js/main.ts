956b0c3624122d88e1c1832f4c818269823f722a
abraham <wole1702@gmail.com>
2024-10-10 01:41:49 +0100

    code

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -60,3 +60,16 @@ function executeWork (employee: Director | Teacher) {
 		}
 }
 
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+		if (todayClass === "Math"){
+				return "Teaching Math";
+		}
+		if (todayClass === "History") {
+				return "Teaching History";
+		}
+}
+
+
+
---
+++ 0x04-TypeScript/task_3/js/crud.d.js
@@ -0,0 +1,8 @@
+import {RowID, RowElement} from './interface.ts';
+
+declare function insertRow(row: number);
+declare function deleteRow(rowId: number);
+declare function updateRow(rowId, row);
+
+
+
